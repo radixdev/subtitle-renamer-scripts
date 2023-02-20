9998095ed678d91cff80b9f68c4021450cc30498
Stephen Townsend <26904641+townsste@users.noreply.github.com>
2023-02-19 22:51:59 -0800

    Add files via upload
    Added logic to allow subtitles renaming to work with TV Shows and not just movies.

--- renamer.py
+++ renamer.py
@@ -1,15 +1,18 @@
 import os
 import sys
 import shutil
+import itertools
 from glob import glob
 from os.path import join
 from os.path import basename
 from difflib import SequenceMatcher
+from collections import defaultdict
 
 def similar(a, b):
   return SequenceMatcher(None, a, b).ratio()
 
 workingDir = str(sys.argv[1])
+
 print("workingDir", workingDir)
 
 # Verify we're in the right spot
@@ -31,10 +34,10 @@ def getLangCode(filename):
   # matches instead of winner take all
   if (similar("_english.srt", n) > 0.9):
     print("matched whole word", n)
-    return "en"
+    return "eng"
   if (similar("_eng.srt", n) > 0.75):
     print("matched partial word", n)
-    return "en"
+    return "eng"
   # Add other language codes here if you like
   return None
 
@@ -66,6 +69,36 @@ def doFileCopy(oldFile, newFile):
   print (".")
   shutil.copyfile(oldFile, newFile)
 
+def multipleSubs(subs, subsPath, lCode):
+  # Logic for mutiple subtitles
+  if (len(subs) == 3):
+    print("3 Subtitles found")
+    doFileCopy(subsPath[0], subs[0] + lCode + ".forced" + ".srt")
+    doFileCopy(subsPath[1], subs[1] + lCode + ".srt")
+    doFileCopy(subsPath[2], subs[2] + lCode + ".sdh" + ".srt")
+  elif (len(subs) == 2):
+    print("2 Subtitles found")
+    # Compare the file size if <= 20kb assume as forced
+    if (os.path.getsize(subsPath[0]) <= 20000):
+      print("Forced Subtitles found")
+      doFileCopy(subsPath[0], subs[0] + lCode + ".forced" + ".srt")
+      doFileCopy(subsPath[1], subs[1] + lCode + ".srt")
+    else:
+      print("SDH Subtitles found")
+      doFileCopy(subsPath[0], subs[0] + lCode + ".srt")
+      doFileCopy(subsPath[1], subs[1] + lCode + ".sdh" + ".srt")
+  elif (len(subs) == 1):
+    print("1 Subtitle found")
+    # Compare the file size if <= 20kb assume as forced
+    if (os.path.getsize(subsPath[0]) <= 20000):
+      doFileCopy(subsPath[0], subs[0] + lCode + ".forced" + ".srt")
+    else:
+      doFileCopy(subsPath[0], subs[0] + lCode + ".srt")
+  else:
+    print("More than 3 Subtitles found")
+    for i in range(len(subs)):
+      doFileCopy(subsPath[i], subs[i] + "(" + i + ")" + lCode + ".srt")
+
 # Fix the movie files
 # C/dir/to/movie/AVENGERS.1080p/
 #   AVENGERS.1080p.mp4
@@ -84,29 +117,13 @@ if (does_working_dir_contains_matching_media(workingDirBasename)):
       print("Can't determine language code for =>", movieBaseName)
       continue
     # Add to list
-    subtitles.append(join(workingDir, workingDirBasename + "." + langCode))
+    subtitles.append(join(workingDir, workingDirBasename + "."))
     subtitlesPath.append(filePath)
+    
+  # Send lists to sub logic function
+  multipleSubs(subtitles, subtitlesPath, langCode)
 else:
   print("This is not a movie directory", workingDir)
-  
-# Logic for mutiple subtitles
-if (len(subtitles) == 3):
-  doFileCopy(subtitlesPath[0], subtitles[0] + ".forced" + ".srt")
-  doFileCopy(subtitlesPath[1], subtitles[1] + ".srt")
-  doFileCopy(subtitlesPath[2], subtitles[2] + ".sdh" + ".srt")
-elif (len(subtitles) == 2):
-  # Compare the file size if <= 20kb assume as forced
-  if (os.path.getsize(subtitlesPath[0]) <= 20000):
-    doFileCopy(subtitlesPath[0], subtitles[0] + ".forced" + ".srt")
-    doFileCopy(subtitlesPath[1], subtitles[1] + ".srt")
-  else:
-    doFileCopy(subtitlesPath[0], subtitles[0] + ".srt")
-    doFileCopy(subtitlesPath[1], subtitles[1] + ".sdh" + ".srt")
-elif (len(subtitles) == 1):
-  doFileCopy(subtitlesPath[0], subtitles[0] + ".srt")
-else:
-  print("More than 3 Subtitles found")
-  
 
 # Files that are 2 layers deep
 showFiles = glob(subsDir + "/**/*.srt")
@@ -117,6 +134,13 @@ showFiles = glob(subsDir + "/**/*.srt")
 #   Subs/
 #     PENTHOUSE.S01E01.1080p/
 #       2_English.srt
+
+# Dictionary to combine subtitles based on directory.
+subtitleDict = {}
+subtitlePathDict = {}
+# Create a Subtitle List to hold the files based on their directory.
+showSubtitles = []
+showSubtitlesPath = []
 for filePath in showFiles:
   showBaseName = basename(filePath)
   langCode = getLangCode(showBaseName)
@@ -124,12 +148,33 @@ for filePath in showFiles:
     # print("Can't determine language code for =>", showBaseName)
     continue
   print("on show file", filePath)
-
+  
   # Do a media match check
   episodeName = basename(os.path.dirname(filePath))
+  print("episodeName", episodeName)
   if (not does_working_dir_contains_matching_media(episodeName)):
     print("no media found for episode", episodeName, "in workingDir", workingDir)
     continue
 
-  newFile = join(workingDir, episodeName + "." + langCode + ".srt")
-  doFileCopy(filePath, newFile)
+  # Add to dictionary to combine files based on directory
+  if episodeName in subtitleDict:
+    # append the new number to the existing array at this slot
+    subtitleDict[episodeName].append(join(workingDir, episodeName + "."))
+    subtitlePathDict[episodeName].append(filePath)
+  else:
+    # create a new array in this slot
+    subtitleDict[episodeName] = [join(workingDir, episodeName + ".")]
+    subtitlePathDict[episodeName] = [filePath]
+    
+# Loop through the two dictionaries to place subtitles into list based on the directory they are in
+for x, x1 in zip(subtitleDict, subtitlePathDict):
+  for y, y1 in zip(subtitleDict[x], subtitlePathDict[x1]):
+    showSubtitles.append(y)
+    showSubtitlesPath.append(y1)
+    
+  # Send lists to sub logic function
+  multipleSubs(showSubtitles, showSubtitlesPath, langCode)
+  # Clear lists for next dir subtitles
+  showSubtitles.clear()
+  showSubtitlesPath.clear()
+
